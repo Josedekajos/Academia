87f173a5e9146cc7f348a1a5cfe26b032cd27771
Josedekajos <josedekajos@gmail.com>
2025-01-15 00:31:01 -0800

    Add files via upload

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,17 +1,29 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './NavHeader.css';
-import PropTypes from 'prop-types';
 
 const NavHeader = ({ userInfo }) => {
+  const navigate = useNavigate();
+
+  const handleChatClick = () => {
+    navigate('/Chat');
+  };
+
   return (
     <nav className="nav-header">
       <div className="nav-brand">
-        <Link to="/">StudyConnect</Link>
+        <Link to="/">Academia</Link>
       </div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/dashboard">Dashboard</Link></li>
         <li><Link to="/about">About</Link></li>
+        <li>
+          <button className="chat-button" onClick={handleChatClick}>
+            Chat
+          </button>
+        </li>
+        <li><Link to="/groups">Groups</Link></li>
       </ul>
       <div className="user-profile">
         <span className="user-name">{userInfo.name}</span>
@@ -21,12 +33,7 @@ const NavHeader = ({ userInfo }) => {
   );
 };
 
-
-NavHeader.propTypes = {
-  userInfo: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-  }).isRequired,
-};
-
 export default NavHeader;
+
+/*
+*/
